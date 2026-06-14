b46020abde2819457d9605181700b7428a841fad
Erfan Khebrati <khebrati@gmail.com>
2026-06-14 15:02:31 +0330

    change deploy path to base

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
-const repoName = "portfolio";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: `/${repoName}`,
   images: { unoptimized: true },
   trailingSlash: true,
 };
